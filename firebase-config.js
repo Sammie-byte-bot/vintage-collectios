e99ff5ad345ac26a9eb61db03463b30f6374cf57
Samuel Munyambu <munyambusamuel72@gmail.com>
2026-06-14 02:32:42 +0300

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCp9B2FEEGrwLxMFKjKkWnSa3t_vX_l_fY",
+  authDomain: "vintage-collections.firebaseapp.com",
+  projectId: "vintage-collections",
+  storageBucket: "vintage-collections.firebasestorage.app",
+  messagingSenderId: "948077303440",
+  appId: "1:948077303440:web:0c44665b48e66871002a15",
+  measurementId: "G-HQZ7C1HYNR",
+};
